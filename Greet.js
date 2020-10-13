c6ed9d04418e6a216815c1028622eee7462a3265
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-10-13 22:16:34 +0530

    Update Greet.js

--- Greet.js
+++ Greet.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello Priyanka</h1>
+//function Greet() {
+   // return <h1>Hello Priyanka</h1>
+//}
+
+const Greet = props  => {
+    console.log(props)
+    return (
+        <div>
+    <h1> 
+        hello {props.name} a.k.a {props.heroName}
+    </h1>
+    {props.children}
+    </div>
+    )
+
 }
 
 export default Greet
