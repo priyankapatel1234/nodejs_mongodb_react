65823090d18ccd07e0f169d06e9bcfe01b941347
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-10-13 22:15:25 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,16 +1,34 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import  './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+import Hello from './components/Hello'
+import Message from './components/Message';
 
-function App() {
-  return (
-    <div className="App">
-    <Greet/>
-    <Welcome/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      < Message/>
+      { /*
+        <Greet name="priyanka"  heroName="batman" >
+        <p> This is children props.</p>
+        </Greet>
+        <Greet name="prachi" heroName="superman" >
+        <button>Submit</button>
+        </Greet>
+        <Greet name="aman"  heroName="Wonder women" />
+       <Welcome name="priyanka"  heroName="batman"/> 
+       <Welcome name="prachi" heroName="superman"/> 
+      <Welcome name="aman"  heroName="Wonder women"/>  */}
+        { /* <Hello /> */}
+      </div>
+    );
+  }
 }
+
+  
 
 export default App;
