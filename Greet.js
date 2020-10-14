6803787374c05b2441580081595f6beb52ce6be4
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-10-14 23:29:33 +0530

    Update Greet.js

--- Greet.js
+++ Greet.js
@@ -3,7 +3,9 @@ import React from 'react'
 //function Greet() {
    // return <h1>Hello Priyanka</h1>
 //}
+/*
 
+ 
 const Greet = props  => {
     console.log(props)
     return (
@@ -16,5 +18,36 @@ const Greet = props  => {
     )
 
 }
+*/
+//Destructut the props functinal components 1 way 
+/*
+const Greet = ({name,heroName}) => {
+    
+    return (
+        <div>
+    <h1> 
+        hello {name} a.k.a {heroName}
+    </h1>
+
+    </div>
+    )
+
+} */
+
+//Destructut the props functinal components 2 way 
+const Greet = props => {
+    const { name,heroName} =props
+    
+    return (
+        <div>
+    <h1> 
+        hello {name} a.k.a {heroName}
+    </h1>
+
+    </div>
+    )
+
+}
+
 
 export default Greet
