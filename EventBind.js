38b182bfb0778a5752659e47098d1b9aab68ebfd
Priyanka Patel <55329362+priyankapatel1234@users.noreply.github.com>
2020-10-14 10:52:09 -0700

    Add files via upload

---
+++ EventBind.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react' //rce
+
+ class EventBind extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             message: 'Hello'
+        }
+
+     //   this.clickHandler= this.clickHandler.bind(this)
+    }
+ /*   clickHandler() {
+        this.setState  ({
+            message:'GoodBye'
+        })
+        console.log(this)
+    }  */
+
+    clickHandler =() => {
+        this.setState({
+            message:'goodbye!'
+        })
+    }
+    
+    render() {
+        return (
+            <div>
+                <div>{this.state.message}</div>
+            {/*  <button onClick={this.clickHandler.bind(this)}>click</button>  */}
+             { /* <button onClick={() => this.clickHandler()}>click</button>  */}
+             <button onClick={this.clickHandler}>click</button>
+            </div>
+        )
+    }
+}
+
+export default EventBind
